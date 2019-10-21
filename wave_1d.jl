733c6af5e782ace540447b5c963af24c553b756b
Yong Peng <24858901+YongPeng25@users.noreply.github.com>
2019-10-21 22:21:58 +0200

    Update wave_1d.jl

--- wave_1d.jl
+++ wave_1d.jl
@@ -1,6 +1,7 @@
 # 1D wave propagation modeling with Spectral Element Method (SEM)
 # This code can be used for learning the basic conception of SEM
-# @author: Peng Yong, China  (yongpeng2013@gmail.com)
+# Author: Peng Yong, China University of Petroleum (East China) 
+# E-mail: yongpeng2013@gmail.com
 # Date: 20/08/2019     
 include("define_derivative_matrix.jl")
 include("source.jl")
